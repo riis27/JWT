aa2919cb4c02451fbb0ad241a9b85cfccbf9fa40
rissa <rissasanmiguel@gmail.com>
2025-03-21 23:51:53 -0400

    finished project

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const authRoutes = require('./routes/authRoutes')
-
+const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
+const {requireAuth} = require('./middleware/authMiddleware');
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
+
+
+
 // view engine
 app.set('view engine', 'ejs');
 
@@ -19,6 +24,26 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 
 // routes
 app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
+app.use(authRoutes);
 
-app.use(authRoutes)
+// //create a cookie
+// app.get('/set-cookies', (req, res)=> {
+//   // res.setHeader('Set-Cookie', 'newUser=true');
+
+//   //does the same as setHeader, looks cleaner
+//   res.cookie('newUser', false);
+//   res.cookie('isEmployee', true, {maxAge: 1000 * 60 * 60 * 24, httpOnly: true});
+//   res.send('You got the cookie, check the Application tab in the inspect tool!')
+// })
+
+
+// //read a cookie
+// app.get('/read-cookies', (req, res)=> {
+
+//   const cookies = req.cookies;
+//   console.log(cookies.newUser)
+
+//   res.json(cookies)
+
+// })
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,22 @@
 //controller actions
-const User = require('../models/User')
-
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
 //handle errors
 const handleErrors = (err) => {
     console.log(err.message, err.code);
     let errors = { email: '', password: '' };
+
+
+    //incorrect email
+    if (err.message === 'incorrect email') {
+        errors.email = 'That email is not registered';
+    }
+
+    //incorrect password
+    if (err.message === 'incorrect password') {
+        errors.password = 'That password is incorrect';
+    }
 
 
     if (err.code = 11000) {
@@ -18,19 +29,33 @@ const handleErrors = (err) => {
             errors[properties.path] = properties.message;
         });
     }
+    return errors;
 }
+
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+    //returns the token
+    return jwt.sign({ id }, 'secret message', { expiresIn: maxAge });
+}
+
 
 module.exports.signup_get = (req, res) => {
     res.render('signup');
 }
+
 module.exports.login_get = (req, res) => {
     res.render('login');
 }
+
+
 module.exports.signup_post = async (req, res) => {
     const { email, password } = req.body;
     try {
         const user = await User.create({ email, password });
-        res.status(201).json(user)
+        const token = createToken(user._id);
+
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+        res.status(201).json({ user: user._id });
     } catch (error) {
         const errors = handleErrors(error);
         res.status(400).json({ errors })
@@ -41,7 +66,14 @@ module.exports.signup_post = async (req, res) => {
 module.exports.login_post = async (req, res) => {
     const { email, password } = req.body;
 
-    console.log(email, password)
-    res.send('user login');
+    try {
+        const user = await User.login(email, password);
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+        res.status(200).json({ user: user._id });
+    } catch (err) {
+        const errors = handleErrors(err);
+        res.status(400).json({ errors });
+    }
 }
 
